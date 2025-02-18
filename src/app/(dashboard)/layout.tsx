d25b16d641333a641c993af0fa157d906f665385
Jayan Paliwal <jayan110105@gmail.com>
2025-02-18 22:38:55 +0530

    Prevent Unauthorized access and added a logout button

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,17 +1,47 @@
 import { SidebarProvider} from "~/components/ui/sidebar";
 import { AppSidebar } from "~/components/app-sidebar";
 import ConditionalSidebarTrigger from "~/components/ConditionalSidebarTrigger";
+import { NavUser } from "~/components/nav-user";
+import { auth } from "~/server/auth";
+import { db } from "~/server/db";
+import { users } from "~/server/db/schema";
+import { eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
+  let userData = null;
+
+  if (session?.user?.id) {
+    // Fetch user from DB using authenticated user's ID
+    const user = await db.select().from(users).where(eq(users.id, session.user.id)).limit(1);
+    userData = user[0] ?? null;
+    console.log(userData?.image);
+  }
+
   return (
     <SidebarProvider>
       <AppSidebar />
       <div className="flex flex-col flex-1">
-        <ConditionalSidebarTrigger />
+        <div className="flex justify-between">
+          <div>{ConditionalSidebarTrigger ? <ConditionalSidebarTrigger /> : null}</div>
+          <div>
+            <NavUser user={{
+                name: userData?.name ?? "Guest",
+                email: userData?.email ?? "guest@example.com",
+                avatar: userData?.image ?? "/default-avatar.png",
+              }} />
+          </div>
+        </div>
         <div className="flex-1 items-end">
           {children}
         </div>
