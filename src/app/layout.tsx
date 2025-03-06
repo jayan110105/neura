4e791905d3533aa89cacd73886eeb1eb3fbc1913
Jayan Paliwal <jayan110105@gmail.com>
2025-03-06 15:04:33 +0530

    Added React Scan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from "~/components/theme-provider";
 import { SessionProvider } from "next-auth/react";
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script src="https://unpkg.com/react-scan/dist/auto.global.js" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
